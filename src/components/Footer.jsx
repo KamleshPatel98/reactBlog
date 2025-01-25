2ec1ff1984c05f27c7ff0fec16ba6eeb426ca8a2
KamleshPatel98 <kamleshp52170@gmail.com>
2025-01-26 00:31:37 +0530

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Footer() {
     return (
-        <div>Footer</div>
+        <footer>
+            <ul className="footer">
+                <li><Link to={'/'}>Home</Link></li>
+                <li><Link to={'/about'}>About</Link></li>
+                <li><Link to={'/contact'}>Contact</Link></li>
+            </ul>
+
+            <div className="footer-container">
+                <small>&copy; Copyright All Rights Reserved || Patel Brothers</small>
+            </div>
+        </footer>
     )
 }
