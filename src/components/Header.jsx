9e9fc7d81cf62daa3693552b77a35551a7c73758
KamleshPatel98 <kamleshp52170@gmail.com>
2025-01-26 00:05:46 +0530

    npm i react-icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import { Link } from "react-router-dom"
+
+// Icon
+import { FaBars } from "react-icons/fa";
+import { AiOutlineClose } from "react-icons/ai";
 
 export default function Header() {
     return (
-        <div>Header</div>
+        <nav>
+            <div className="container nav-container">
+                <Link to={'/'}>
+                    <h3>Patel Brothers</h3>
+                </Link>
+                <ul className=" d-flex">
+                    <li><Link to={'/about'}>About</Link></li>
+                    <li><Link to={'/blog'}>Blog</Link></li>
+                    <li><Link to={'/contact'}>Contact</Link></li>
+                    <li><Link to={'/register'}>Register</Link></li>
+                    <li><Link to={'/login'}>Login</Link></li>
+                </ul>
+
+                <button class="nav-toggle-button">
+                    <FaBars />
+                    <AiOutlineClose />
+                </button>
+            </div>
+        </nav>
     )
 }
