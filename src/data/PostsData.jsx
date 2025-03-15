3279be2c401b381622b27824f7e03d7a1f580d01
KamleshPatel98 <kamleshp52170@gmail.com>
2025-03-15 19:11:11 +0530

    reduce slider data

--- src/data/PostsData.jsx
+++ src/data/PostsData.jsx
@@ -24,44 +24,5 @@ export const PostsData = [
         authorId: 1,
         title: 'lorem wds sdd sdc',
         description: 'dhfufui jsjhi ihfiuh ifhuih jhsjh jhjijw jbjjhwe hifiw jhfj jjnfijh ni'
-    },
-    {
-        id: 1,
-        image: blogImage1,
-        category: 'Education',
-        authorId: 1,
-        title: 'lorem wds sdd sdc',
-        description: 'dhfufui jsjhi ihfiuh ifhuih jhsjh jhjijw jbjjhwe hifiw jhfj jjnfijh ni'
-    }, {
-        id: 1,
-        image: blogImage1,
-        category: 'Education',
-        authorId: 1,
-        title: 'lorem wds sdd sdc',
-        description: 'dhfufui jsjhi ihfiuh ifhuih jhsjh jhjijw jbjjhwe hifiw jhfj jjnfijh ni'
-    },
-    {
-        id: 1,
-        image: blogImage1,
-        category: 'Education',
-        authorId: 1,
-        title: 'lorem wds sdd sdc',
-        description: 'dhfufui jsjhi ihfiuh ifhuih jhsjh jhjijw jbjjhwe hifiw jhfj jjnfijh ni'
-    },
-    {
-        id: 1,
-        image: blogImage1,
-        category: 'Education',
-        authorId: 1,
-        title: 'lorem wds sdd sdc',
-        description: 'dhfufui jsjhi ihfiuh ifhuih jhsjh jhjijw jbjjhwe hifiw jhfj jjnfijh ni'
-    },
-    {
-        id: 1,
-        image: blogImage1,
-        category: 'Education',
-        authorId: 1,
-        title: 'lorem wds sdd sdc',
-        description: 'dhfufui jsjhi ihfiuh ifhuih jhsjh jhjijw jbjjhwe hifiw jhfj jjnfijh ni'
-    },
+    }
 ]
